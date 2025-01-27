26f880a98cb936309ea7b791e52d8ffc99bda7bc
Jonathan Parker <jonathan.parker@glia.com>
2025-01-27 14:40:23 -0500

    random changes to migrate to new pc

--- assets/scripts/parker_custom.js
+++ assets/scripts/parker_custom.js
@@ -1,26 +1,31 @@
-sm.getApi({version: 'v1'}).then(function(glia) {
+var glia;
 
-    function addSubmitListener(engagement) {
-      var submit = document.querySelector('#sign-up_btn');
-  
-      submit.addEventListener('click', function() {
-        var name = document.querySelector('#name').value;
-        var email = document.querySelector('#email').value;
-        engagement.recordEvent({message: name + ' signed up with email ' + email});
-      });
-    }
-  
-    glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, addSubmitListener);
-
-    const ddlTeam = document.querySelector("#ddl_team");
-
-    ddlTeam.addEventListener('change', function() {
-      glia.updateInformation({
-        customAttributes: {
-          'fav-football-team': ddlTeam.value
-        }
-      })
-    })
+sm.getApi({ version: 'v1' }).then(function (api) {
+  glia = api;
+  glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, addSubmitListener);
+  glia.addEventListener(glia.EVENTS.ENGAGEMENT_REQUEST, engagementRequested);
 });
 
+function engagementRequested(engagement) {
+  debugger;
+}
 
+function addSubmitListener(engagement) {
+  var submit = document.querySelector('#sign-up_btn');
+
+  submit.addEventListener('click', function () {
+    var name = document.querySelector('#name').value;
+    var email = document.querySelector('#email').value;
+    engagement.recordEvent({ message: name + ' signed up with email ' + email });
+  });
+}
+
+const ddlTeam = document.querySelector("#ddl_team");
+
+ddlTeam.addEventListener('change', function () {
+  glia.updateInformation({
+    customAttributes: {
+      'fav-football-team': ddlTeam.value
+    }
+  })
+});
