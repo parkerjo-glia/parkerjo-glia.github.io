fd66a0a45eed7f4d869e0e3862f905b9607d3ca2
Jonathan Parker <jonathan.parker@glia.com>
2025-03-28 12:49:47 -0400

    added alert inside sm promise

--- assets/scripts/auto_engage.js
+++ assets/scripts/auto_engage.js
@@ -93,6 +93,7 @@ const conversation = {
 
 function wireUp() {
     sm.getApi({ version: 'v1' }).then(function (api) {
+        alert(`Glia Loaded Successfully`);
         glia = api;
         glia.queueForEngagement('text', { queueId: '76dbea19-92f7-4e8b-8c6d-de99280b7bcd' })
         glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, engagementStarted);
