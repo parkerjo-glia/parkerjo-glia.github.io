3794547a3aa9093be954489ed2b3c92c5c3f7919
Jonathan Parker <jonathan.parker@glia.com>
2025-03-28 11:31:10 -0400

    try and catch getapi error

--- assets/scripts/auto_engage.js
+++ assets/scripts/auto_engage.js
@@ -91,19 +91,8 @@ const conversation = {
     ]
  };
 
-
-
-if(sm) {
-    if (sm.getApi) {
-        alert(sm.getApi);
-    } else {
-        alert("getAPI undefined");
-    }
-} else {
-    alert("SM is undefined");
-}
-
 sm.getApi({ version: 'v1' }).then(function (api) {
+    alert("Glia Loaded");
   glia = api;
   glia.queueForEngagement('text', { queueId: '76dbea19-92f7-4e8b-8c6d-de99280b7bcd' })
   glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, engagementStarted);
