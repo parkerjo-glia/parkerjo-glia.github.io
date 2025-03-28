4f96ddc41f33f00bcb5a5305ea508417791be04b
Jonathan Parker <jonathan.parker@glia.com>
2025-03-28 11:54:25 -0400

    removed debug lines

--- assets/scripts/auto_engage.js
+++ assets/scripts/auto_engage.js
@@ -91,18 +91,6 @@ const conversation = {
     ]
  };
 
-
-
-if(sm) {
-    if (sm.getApi) {
-        alert(`${sm.getApi}`);
-    } else {
-        alert("getAPI undefined");
-    }
-} else {
-    alert("SM is undefined");
-}
-
 sm.getApi({ version: 'v1' }).then(function (api) {
   glia = api;
   glia.queueForEngagement('text', { queueId: '76dbea19-92f7-4e8b-8c6d-de99280b7bcd' })
