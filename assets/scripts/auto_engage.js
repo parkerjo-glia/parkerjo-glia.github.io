51fe52d5ebf64344efedc3bdd51de6d53720a7a5
Jonathan Parker <jonathan.parker@glia.com>
2025-04-02 15:04:09 -0400

    more auto engagement updates

--- assets/scripts/auto_engage.js
+++ assets/scripts/auto_engage.js
@@ -1,29 +1,40 @@
 var gliaAPI
-var conversation;
 var messageNdx = 0;
+var conversation;
 var visitor;
 var currentEngagement;
 
-var scriptCatalog = [];
-var visitorCatalog = [];
+var scriptCatalog;
+var visitorCatalog;
+const scriptCatalogURL = "https://parkerjo-glia.github.io/assets/json/script_catalog.json";
+const visitorCatalogURL = "https://parkerjo-glia.github.io/assets/json/visitors_catalog.json";
 
+/*
 $.getJSON('./assets/json/script_catalog.json', data => {
     scriptCatalog = data;
 });
 $.getJSON('./assets/json/visitors_catalog.json', data => {
     visitorCatalog = data;
 });
+*/
 
-function syntheticEngagement() {
+async function syntheticEngagement(scriptId) {
+
+    console.log("loading catalogs");
+    await loadCatalogs();
+    console.log("catalogs loaded");
+
     sm.getApi({ version: 'v1' }).then(function (glia) {
+        gliaAPI = glia;
         messageNdx = 0;
-        const scriptNdx = getRandomNum(scriptCatalog.length - 1);
+        const scriptNdx = scriptId ?? getRandomNum(scriptCatalog.length - 1);
         const visitorNdx = getRandomNum(visitorCatalog.length - 1);
         conversation = scriptCatalog[scriptNdx];
         visitor = visitorCatalog[visitorNdx];
 
-        gliaAPI = glia;
-        glia.queueForEngagement('text', { queueId: '76dbea19-92f7-4e8b-8c6d-de99280b7bcd' })
+        var queue_id = '8c458838-cb59-4d6c-ae31-d04fd2bbda1f'; // func you up
+        //var queue_id = '76dbea19-92f7-4e8b-8c6d-de99280b7bcd'; // GVA Queue
+        glia.queueForEngagement('text', { queueId: queue_id })
         glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, engagementStarted);
     }).catch(e => {
         alert(`Error: ${e}`);
@@ -39,13 +50,15 @@ function engagementStarted(engagement) {
 function messageReceived(messages) {
     const lastMessage = messages[0];
 
-    if (lastMessage.sender == 'operator' || (lastMessage.sender == 'visitor' && lastMessage.status == 'delivered')) {
+    if (lastMessage.sender == 'operator') {
+        console.log(`Received Operator Message for step ${messageNdx} :: ${JSON.stringify(lastMessage)}`);
         messageNdx = messageNdx + 1;
-        processChatMessage();
+        setTimeout(processChatMessage, 500);
     }
 }
 
 function processChatMessage() {
+    console.log(`Processing Step ${messageNdx}`);
 
     if (messageNdx >= conversation.messages.length) {
         currentEngagement.end().then(() => {
@@ -53,46 +66,53 @@ function processChatMessage() {
         });
     } else {
 
-        gliaAPI.updateInformation({
-            name: visitor.name,
-            phone: visitor.phone,
-            email: visitor.email,
-            customAttributesUpdateMethod: 'merge',
-            customAttributes: {
-                visitor_id: `${visitor.id}`,
-                conversation_id: `${conversation.id}`,
-                step_in_conversation: `${messageNdx}`
-            }
-        }).then(() => {
+        const nextMsg = conversation.messages[messageNdx];
+        console.log(`Processing Step ${messageNdx} :: ${nextMsg.sender} - ${nextMsg.type}`);
 
-            const nextMsg = conversation.messages[messageNdx];
+        switch (nextMsg.type) {
+            case 'visitor-msg':
+                const timeout = getRandomNum(500);
+                messageNdx = messageNdx + 1;
 
-            switch (nextMsg.type) {
-                case 'visitor-msg':
-                    const charNum = nextMsg.message.length;
-                    const timeout = charNum * getRandomNum(25);
-
-                    setTimeout(currentEngagement.chat.sendMessage, timeout, formatMessage(nextMsg.message, visitor));
-                    break;
-                case 'visitor-wait':
-                    messageNdx = messageNdx + 1
-                    setTimeout(processChatMessage, nextMsg.timeout)
-                case 'operator-wait':
-                    messageNdx = messageNdx + 1
-                    processChatMessage();
-                default:
-                    break;
-            }
-        }).catch(err => {
-
-        });
+                gliaAPI.updateInformation({
+                    name: visitor.name,
+                    phone: visitor.phone,
+                    email: visitor.email,
+                    customAttributesUpdateMethod: 'merge',
+                    customAttributes: {
+                        visitor_id: `${visitor.id}`,
+                        conversation_id: `${conversation.id}`,
+                        step_in_conversation: `${messageNdx}`
+                    }
+                }).then(() => {
+                    setTimeout(sendMessage, timeout, formatMessage(nextMsg.message, visitor));
+                }).catch(err => {
+                    alert(`Error: ${e}`);
+                });
+                break;
+            case 'visitor-wait':
+                console.log(`Waiting at step ${messageNdx}, waiting for ${nextMsg.timeout}`);
+                messageNdx = messageNdx + 1;
+                setTimeout(processAfterWait, nextMsg.timeout);
+                break;
+            case 'operator-wait':
+                messageNdx = messageNdx + 1;
+                processChatMessage();
+                break;
+            default:
+                break;
+        }
     }
 }
 
 function sendMessage(message) {
     currentEngagement.chat.sendMessage(message, null);
-    messageNdx = messageNdx + 1;
     processChatMessage();
+}
+
+function processAfterWait() {
+    console.log(`Wait is over, begin processing msg ${messageNdx}`);
+    processChatMessage()
 }
 
 function getRandomNum(maxNum) {
@@ -105,4 +125,36 @@ function formatMessage(str, data) {
         str = str.replace(regex, data[key]);
     }
     return str;
+}
+
+async function loadCatalogs() {
+    await Promise.all[
+        getData(scriptCatalog, scriptCatalogURL),
+        getData(visitorCatalog, visitorCatalogURL)
+    ];
+}
+
+async function getData(catalog, url) {
+    try {
+        console.log(`loading catalog from ${url}`);
+
+        if (!catalog) {
+
+            console.log(`loading catalog from ${url} - fetch`);
+            const response = await fetch(url);
+            console.log(`loading catalog from ${url} - fetch complete`);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            catalog = await response.json();
+            console.log('Data received:', data);
+        }
+
+        console.log(`loading catalog from ${url} :: Complete`);
+        return catalog;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
 }
